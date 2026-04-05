b05f4bc80e771cdc2b0a872757ffe1ea8f8642b7
Abdullahi Ramadan <ramadanayinla93@gmail.com>
2026-04-05 21:27:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,9 @@ app.get("/", (req, res) => {
 
 // ================= EMAIL SYSTEM =================
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // TLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -108,8 +110,7 @@ text:`Welcome to Morcaz Uloom Portal
 Student ID: ${studentId}
 Password: ${passwordPlain}
 
-Login here:
-http://localhost:5000`
+Login here: https://morcaz-uloom-ejigbo-ng.onrender.com`
 
 })
 
